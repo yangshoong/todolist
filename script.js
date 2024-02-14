032b0debfbe1edde2543559e04aecacca834f56d
yangshoong <crinaeae83@gmail.com>
2024-02-15 00:13:40 +0900

    + check, delete

--- script.js
+++ script.js
@@ -2,34 +2,80 @@
 // + 버튼을 클릭하면, 할일이 추가된다
 // delete 버튼을 누르면 할일이 삭제된다
 // check 버튼을 누르면 할일이 끝나면서 밑줄이 간다
+// 1. check 버튼을 클릭하는 순간 true를false로
+// 2. true이면 끝난걸로 간주하고 및줄
+// 3. false이면 안끝난걸로 간주하고 그대로
 // 진행중 끝남 탭을 누르면, 언더바가 이동한다.
 // 끝남탭은, 끝난 아이템만, 진행중 탭은 진행중인 아이템만
 // 전체탭을 누르면 다시 전체 아이템으로 돌아옴.
 
 let taskInput = document.getElementById("task-input");
 let addButton = document.getElementById("add-button");
+let tabs = document.querySelectorAll(".task-tabs div");
 let taskList = [];
 addButton.addEventListener("click", addTask);
 
+
+
 function addTask() {
-  let taskContent = taskInput.value;
-  taskList.push(taskContent);
+  let task = {
+    id: randomIDGenerate(),
+    taskContent: taskInput.value,
+    isComplete: false,
+  };
+  taskList.push(task);
   console.log(taskList);
   render();
 }
 
 function render() {
-  let resultHTML = '';
+  let resultHTML = "";
   for (let i = 0; i < taskList.length; i++) {
-    resultHTML += `<div class="task">
-    <div>집가기</div>
+    if (taskList[i].isComplete == true) {
+      resultHTML += `<div class="task">
+      <div class="task-done">${taskList[i].taskContent}</div>
+      <div>
+        <button onClick="toggleComplete('${taskList[i].id}')">
+        <button onClick="deleteTask('${taskList[i].id}')">Delete</button>
+      </div>
+    </div>`;
+    } else {
+      resultHTML += `<div class="task">
+    <div>${taskList[i].taskContent}</div>
     <div>
-      <button>Check</button>
-      <button>Delete</button>
+      <button onClick="toggleComplete('${taskList[i].id}')">Check</button>
+      <button onClick="deleteTask('${taskList[i].id}')">Delete</button>
     </div>
   </div>`;
-      
+    }
   }
 
   document.getElementById("task-board").innerHTML = resultHTML;
+}
+
+function toggleComplete(id) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id == id) {
+      taskList[i].isComplete = !taskList[i].isComplete;
+      break;
+    }
+  }
+  render();
+  console.log(taskList);
+}
+
+function deleteTask(id) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id == id) {
+      taskList.splice(i, 1);
+      break;
+    }
+  }
+  render();
+}
+
+function randomIDGenerate() {
+  return (
+    Date.now().toString(36) + Math.random().toString(36).substr(2, 5)
+  ).toUpperCase();
 }
