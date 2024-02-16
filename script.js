13c4215a19ab61883428e58c5d42084642247f24
yangshoong <crinaeae83@gmail.com>
2024-02-17 00:05:17 +0900

    repair addtask, deletetask

--- script.js
+++ script.js
@@ -35,6 +35,92 @@ let mode = "all";
 let filterList = [];
 let list = [];
 
+
+function render() {
+  list = [];
+  if (mode === "all") {
+    list = taskList;
+  } else if (mode === "ongoing") {
+    list = filterList;
+  } else if (mode === "done") {
+    list = filterList;
+  }
+
+  let resultHTML = "";
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].isComplete == true) {
+      resultHTML += `<div class="task task-done-line" >
+      <div class="task-done">${list[i].taskContent}</div>
+      <div>
+        <img src="images/undo.png" alt="Check" class="task-button-img" onClick="toggleComplete('${list[i].id}')"/>
+        <img src="images/trash.png" alt="Delete" class="delete-button-img" onClick="deleteTask('${list[i].id}')"/>
+      </div>
+    </div>`;
+    } else if (list[i].isComplete == false) {
+      resultHTML += `<div class="task">
+      <div>${list[i].taskContent}</div>
+      <div>
+      <img src="images/check.png" alt="Check" class="task-button-img" onClick="toggleComplete('${list[i].id}')"/>
+      <img src="images/trash.png" alt="Delete" class="delete-button-img" onClick="deleteTask('${list[i].id}')"/>
+      </div>
+    </div>`;
+    } else {
+      resultHTML += `<div class="task">
+    <div>${list[i].taskContent}</div>
+    <div>
+    <img src="images/check.png" alt="Check" class="task-button-img" onClick="toggleComplete('${list[i].id}')"/>
+    <img src="images/trash.png" alt="Delete" class="delete-button-img" onClick="deleteTask('${list[i].id}')"/>
+    </div>
+  </div>`;
+    }
+  }
+
+  document.getElementById("task-board").innerHTML = resultHTML;
+}
+
+function filter(event) {
+  mode = event.target.id;
+  filterList = [];
+  if (mode === "all") {
+    // render();
+  } else if (mode === "ongoing") {
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete === false) {
+        filterList.push(taskList[i]);
+      }
+    }
+    // render();
+  } else if (mode === "done") {
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete === true) {
+        filterList.push(taskList[i]);
+      }
+    }
+    // render();
+  } 
+  // render();
+}
+
+function randomIDGenerate() {
+  return (
+    Date.now().toString(36) + Math.random().toString(36).substr(2, 5)
+  ).toUpperCase();
+}
+
+
+function addTask() {
+  let task = {
+    id: randomIDGenerate(),
+    taskContent: taskInput.value,
+    isComplete: false,
+  };
+  taskList.push(task);
+  filterList=taskList.filter(task => task.isComplete==false);
+  taskInput.value = '';
+  render();
+}
+
+
 addButton.addEventListener("click", function (event) {
   if (taskInput.value.trim() === "") {
     alert("내용을 입력해주세요");
@@ -64,58 +150,6 @@ for (let i = 1; i < tabs.length; i++) {
 }
 
 
-function addTask() {
-  let task = {
-    id: randomIDGenerate(),
-    taskContent: taskInput.value,
-    isComplete: false,
-  };
-  taskList.push(task);
-  taskInput.value = '';
-  render();
-}
-
-function render() {
-  list = [];
-  if (mode === "all") {
-    list = taskList;
-  } else if (mode === "ongoing") {
-    list = filterList;
-  } else if (mode === "done") {
-    list = filterList;
-  }
-  let resultHTML = "";
-  for (let i = 0; i < list.length; i++) {
-    if (list[i].isComplete == true) {
-      resultHTML += `<div class="task task-done-line" >
-      <div class="task-done">${list[i].taskContent}</div>
-      <div>
-        <img src="images/undo.png" onClick="toggleComplete('${list[i].id}')"/>
-        <img src="images/trash.png" onClick="deleteTask('${list[i].id}')"/>
-      </div>
-    </div>`;
-    } else if (list[i].isComplete == false) {
-      resultHTML += `<div class="task">
-      <div>${list[i].taskContent}</div>
-      <div>
-      <img src="images/check.png" onClick="toggleComplete('${list[i].id}')"/>
-      <img src="images/trash.png" onClick="deleteTask('${list[i].id}')"/>
-      </div>
-    </div>`;
-    } else {
-      resultHTML += `<div class="task">
-    <div>${list[i].taskContent}</div>
-    <div>
-    <img src="images/check.png" alt="Check" class="task-button-img" onClick="toggleComplete('${list[i].id}')"/>
-    <img src="images/trash.png" onClick="deleteTask('${list[i].id}')"/>
-    </div>
-  </div>`;
-    }
-  }
-
-  document.getElementById("task-board").innerHTML = resultHTML;
-}
-
 function toggleComplete(id) {
   for (let i = 0; i < taskList.length; i++) {
     if (taskList[i].id == id) {
@@ -130,36 +164,9 @@ function deleteTask(id) {
   for (let i = 0; i < taskList.length; i++) {
     if (taskList[i].id == id) {
       taskList.splice(i, 1);
+      filterList=filterList.filter(task => task.id!==id);
       break;
     }
   }
   render();
-}
-
-function filter(event) {
-  mode = event.target.id;
-  filterList = [];
-  if (mode === "all") {
-    render();
-  } else if (mode === "ongoing") {
-    for (let i = 0; i < taskList.length; i++) {
-      if (taskList[i].isComplete === false) {
-        filterList.push(taskList[i]);
-      }
-    }
-    render();
-  } else if (mode === "done") {
-    for (let i = 0; i < taskList.length; i++) {
-      if (taskList[i].isComplete === true) {
-        filterList.push(taskList[i]);
-      }
-    }
-    render();
-  }
-}
-
-function randomIDGenerate() {
-  return (
-    Date.now().toString(36) + Math.random().toString(36).substr(2, 5)
-  ).toUpperCase();
 }
